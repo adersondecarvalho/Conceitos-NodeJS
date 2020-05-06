28dca3588a4b6699161db1a3de8033e66c565e58
Aderson de Carvalho <acarvalho@inf.ufsm.br>
2020-05-05 21:46:35 -0300

    Change Date

--- src/app.js
+++ src/app.js
@@ -14,22 +14,25 @@ const weatherStations = [
     id: 1, 
     name:'Santa Maria',
     timezone: 'America/Fortaleza',
-    latitude: '-29.6842',
-    longitude: '-53.8069,113',
+    latitude: -29.6842,
+    longitude: -53.8069,
+    altitude: 113,
   },
   {
     id: 2,
     name: 'Budapest',
     timezone: 'Europe/Budapest',
-    latitude: '47.498',
-    longitude: '19.0399,527',
+    latitude: 47.498,
+    longitude: 19.0399,
+    altitude: 527,
   },
   {
     id: 3,
     name: 'Santiago',
     timezone: 'America/Santiago',
-    latitude: '-33.4592',
-    longitude: '-70.6453,570',
+    latitude: -33.4592,
+    longitude: -70.6453,
+    altitude: 570,
   }
 ];
 
@@ -97,5 +100,18 @@ app.post("/weather-data/:id", (request, response) => {
 
   return response.json(weatherStations[stationIndex]);
 });
+/*
+CREATE TABLE `weather_data_1` (
+  `id` int(11) NOT NULL,
+  `air_temperature` float NOT NULL,
+  `air_humidity` int(11) DEFAULT NULL,
+  `wind_speed` float NOT NULL,
+  `rainfall` float NOT NULL DEFAULT '0',
+  `moment` datetime NOT NULL,
+  PRIMARY KEY (`id`,`moment`),
+  CONSTRAINT `weather_data_1_FK` FOREIGN KEY (`id`) REFERENCES `weather_stations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+;
+*/
 
 module.exports = app;
